5ee58b4a5b41acf0d247a1fcff5a23e307dea793
akashkumar7005 <31045135+akashkumar7005@users.noreply.github.com>
2021-05-17 12:44:51 +0530

    sidebar updated

--- Google_faqs/js/faq.js
+++ Google_faqs/js/faq.js
@@ -7,21 +7,30 @@ accordionItemHeaders.forEach(accordionItemHeaders=>{
 });
 
 
+const sectionImage=document.querySelector('#image-section');
+const accordionSection=document.querySelector('#accordion-section');
 
 
-
-function openNav() {
+ openNav=()=> {
     document.getElementById("mySidebar").style.width = "300px";
     document.getElementById("main").style.marginLeft = "300px";
     document.querySelector(".brand-logo-desktop").classList.add("hide");
     document.querySelector(".openbtn").classList.add("hide");
+    sectionImage.addEventListener('click',reset);
+    accordionSection.addEventListener('click',reset);
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function closeNav() {
+closeNav=()=> {
 
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+   
+  }
+
+  reset=()=>
+  {
+    closeNav();  
     document.querySelector(".brand-logo-desktop").classList.remove("hide");
     document.querySelector(".openbtn").classList.remove("hide");
   }
