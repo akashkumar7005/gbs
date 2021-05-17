faa626e5813ff60fbac5e55daba5ed6421e7a1d9
akashkumar7005 <31045135+akashkumar7005@users.noreply.github.com>
2021-05-17 17:40:57 +0530

    For Review

--- Google_faqs/js/faq.js
+++ Google_faqs/js/faq.js
@@ -1,36 +1,35 @@
-const accordionItemHeaders=document.querySelectorAll(".accordion-item-header");
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+const sectionImage = document.querySelector('#image-section');
+const accordionSection = document.querySelector('#accordion-section');
 
-accordionItemHeaders.forEach(accordionItemHeaders=>{
-    accordionItemHeaders.addEventListener("click",event=>{
-        accordionItemHeaders.classList.toggle("active");
-    });
+/* Hiding and Showing the accordian */
+accordionItemHeaders.forEach(accordionItemHeaders => {
+  accordionItemHeaders.addEventListener("click", event => {
+    accordionItemHeaders.classList.toggle("active");
+  });
 });
 
 
-const sectionImage=document.querySelector('#image-section');
-const accordionSection=document.querySelector('#accordion-section');
+/* Opening the Sidebar */
+openNav = () => {
+  document.getElementById("mySidebar").style.width = "310px";
+  document.getElementById("main").style.marginLeft = "310px";
+  document.querySelector(".brand-logo-desktop").classList.add("hide");
+  document.querySelector(".sidebar-openbtn").classList.add("hide");
+  sectionImage.addEventListener('click', reset);
+  accordionSection.addEventListener('click', reset);
+}
 
+/* Closing the Sidebar */
+closeNav = () => {
 
- openNav=()=> {
-    document.getElementById("mySidebar").style.width = "300px";
-    document.getElementById("main").style.marginLeft = "300px";
-    document.querySelector(".brand-logo-desktop").classList.add("hide");
-    document.querySelector(".openbtn").classList.add("hide");
-    sectionImage.addEventListener('click',reset);
-    accordionSection.addEventListener('click',reset);
-  }
-  
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-closeNav=()=> {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
 
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-   
-  }
-
-  reset=()=>
-  {
-    closeNav();  
-    document.querySelector(".brand-logo-desktop").classList.remove("hide");
-    document.querySelector(".openbtn").classList.remove("hide");
-  }
+/* Resetting the Header logo when sidebar is closed */
+reset = () => {
+  closeNav();
+  document.querySelector(".brand-logo-desktop").classList.remove("hide");
+  document.querySelector(".sidebar-openbtn").classList.remove("hide");
+}
